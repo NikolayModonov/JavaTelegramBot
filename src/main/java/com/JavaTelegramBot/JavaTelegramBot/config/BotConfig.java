14d785acce67dd0d3bafea9d63d5a4343b9ef9eb
NikolayModonov <nregspam@gmail.com>
2023-03-27 20:44:43 +0500

    APPLICATION FAILED TO START. Failed to configure a DataSource: 'url' attribute is not specified...

--- src/main/java/com/JavaTelegramBot/JavaTelegramBot/config/BotConfig.java
+++ src/main/java/com/JavaTelegramBot/JavaTelegramBot/config/BotConfig.java
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@Data
 @PropertySource("application.properties")
+@Data
 public class BotConfig {
 
     @Value("${bot.name}")
